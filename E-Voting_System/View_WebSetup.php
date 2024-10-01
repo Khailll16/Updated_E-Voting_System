5b46fd058f6ac9933fdd53878279d9f8ae75f1cd
Mikhail <mikhailvillanueva0216@gmail.com>
2024-10-02 01:12:48 +0800

    mikhail latest update

--- E-Voting_System/View_WebSetup.php
+++ E-Voting_System/View_WebSetup.php
@@ -82,10 +82,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['admin_username'])) {
                                     </div>
 
                                     <div class="back-button">
-                                        <button class="button-back"><a href="Dashboard_Page.php" style="color: white;"><i
-                                                    class='bx bx-arrow-back'></i>Back</a></button>
-                                        <button class="button-edit"><a href="Edit_WebSetup.php?id=<?php echo $row['id']; ?>" style="color: #24724D;"><i
-                                                    class='bx bx-edit'></i>Update</a></button>
+                                        <button class="button-add"><a href="Edit_WebSetup.php?id=<?php echo $row['id']; ?>" style="color: white; display:flex; align-items:center; gap: 3px;"><i class='bx bx-edit'></i>Update</a></button>
                                     </div>
 
                                     <div id="addvoters-popup" class="addvoters-popup">
@@ -93,187 +90,187 @@ if (isset($_SESSION['id']) && isset($_SESSION['admin_username'])) {
                                         <div class="addvoters-popup-forms">
                                             <form action="" method="POST">
                                                 <div class="container">
-                                                        <div class="profile-section">
-                                                            <p style="font-size: 24px; font-weight: bold; color: #4A4A4A;">Logo</p>
-                                                            <div class="image-container">
-                                                                <img id="profile-picture" src="Organization/<?php echo $row['logo'] ?>" alt="" style="background-color: white;">
-                                                            </div>
-                                                            <input type="file" id="file-input" name="orglogo" class="file-input" accept="image/*" onchange="previewImage(event)">
+                                                    <div class="profile-section">
+                                                        <p style="font-size: 24px; font-weight: bold; color: #4A4A4A;">Logo</p>
+                                                        <div class="image-container">
+                                                            <img id="profile-picture" src="Organization/<?php echo $row['logo'] ?>" alt="" style="background-color: white;">
                                                         </div>
-
-                                                        <div class="form-section">
-
-                                                            <label for="" style="text-align: start; margin-left: 25px; margin-bottom: 10px;">Name of organization</label>
-                                                            <input style = "border:1px solid #24724D;" type="text" name="orgname" class="input-field" value="<?php echo $row['organization_name'] ?>" disabled>
-
-                                                            <label for="" style="text-align: start; margin-left: 25px; margin-bottom: 10px;">Number</label>
-                                                            <input style = "border:1px solid #24724D;" type="text" name="number" class="input-field" value="<?php echo $row['admin_number'] ?>" disabled>
-
-                                                            <label for="" style="text-align: start; margin-left: 25px; margin-bottom: 10px;">Address</label>
-                                                            <input style = "border:1px solid #24724D;" type="text" name="address" class="input-field" value="<?php echo $row['school_address'] ?>" disabled>
-
-                                                        </div>
-                                                    <?php
-                                                }
-                                                    ?>
+                                                        <input type="file" id="file-input" name="orglogo" class="file-input" accept="image/*" onchange="previewImage(event)">
                                                     </div>
-                                                </div>
-                                            </form>
 
+                                                    <div class="form-section">
+
+                                                        <label for="" style="text-align: start; margin-left: 25px; margin-bottom: 10px;">Name of organization</label>
+                                                        <input style="border:1px solid #24724D;" type="text" name="orgname" class="input-field" value="<?php echo $row['organization_name'] ?>" disabled>
+
+                                                        <label for="" style="text-align: start; margin-left: 25px; margin-bottom: 10px;">Number</label>
+                                                        <input style="border:1px solid #24724D;" type="text" name="number" class="input-field" value="<?php echo $row['admin_number'] ?>" disabled>
+
+                                                        <label for="" style="text-align: start; margin-left: 25px; margin-bottom: 10px;">Address</label>
+                                                        <input style="border:1px solid #24724D;" type="text" name="address" class="input-field" value="<?php echo $row['school_address'] ?>" disabled>
+
+                                                    </div>
+                                                <?php
+                                            }
+                                                ?>
+                                                </div>
                                         </div>
+                                        </form>
 
                                     </div>
 
-
                             </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+
+            <!-----SIDE BAR------>
+            <nav class="sidebar">
+
+                <!-----MENU BAR------>
+                <div class="menu-bar">
+                    <?php
+                    $sql = "SELECT * FROM `setup`";
+                    $result = $conn->query($sql);
+
+                    if (!$result) {
+                        die("Invalid query: " . $conn->error);
+                    } else {
+                        ($row = mysqli_fetch_assoc($result));
+                    }
+                    ?>
+
+                    <!-----SIDEBAR TOP CONTENT-->
+                    <div class="sidebar-content">
+                        <div class="sidebar-top-content">
+
+                            <!------SIKHAY LOGO-->
+                            <div class="sikhay-logo">
+                                <img src="Organization/<?php echo $row['logo'] ?>" alt="" width="55px">
+                                <div class="school-name">
+                                    <p style="color: #4A4A4A; font-size: 14px;"><?php echo $row['organization_name']; ?></p>
+                                    <p style="font-weight: lighter; font-size: 13px; color: #9F9898;">Organization</p>
+                                </div>
+                            </div>
+
+                            <!-----PROFILE ADMIN------>
+                            <header class="sidebar-profile">
+                                <div class="image-text">
+                                    <?php
+                                    $sql = "SELECT * FROM `admin`";
+                                    $result = $conn->query($sql);
+
+                                    if (!$result) {
+                                        die("Invalid query: " . $conn->error);
+                                    } else {
+                                        ($row = mysqli_fetch_assoc($result))
+
+                                    ?>
+                                        <span class="image">
+                                            <img id="picture-admin" src="Images/<?php echo $row['admin_profile'] ?>" alt="">
+                                        </span>
+                                        <div class="text header-text">
+                                            <p id="name-admin"><?php echo $row['firstname']; ?> <?php echo $row['lastname']; ?></p>
+                                            <span class="name">Online</span>
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                            </header>
 
                         </div>
                     </div>
+
+
+                    <!-----MENU------>
+                    <div class="menu">
+
+                        <!-----MENU LINKS------>
+                        <ul class="menu-links">
+
+                            <!-----DASHBOARD------>
+                            <li class="nav-link">
+                                <a href="Dashboard_Page.php">
+                                    <i class='bx bxs-dashboard icon'></i>
+                                    <span class="text nav-text">DashBoard</span>
+                                </a>
+                            </li>
+
+                            <!-----VOTES------>
+                            <li class="nav-link">
+                                <a href="Votes_Page_Admin.php">
+                                    <i class='bx bxs-box icon'></i>
+                                    <span class="text nav-text">Votes</span>
+                                </a>
+                            </li>
+
+                            <!-----Sections------>
+                            <li class="nav-link">
+                                <a href="Section_Page_Admin.php">
+                                    <i class='bx bxs-objects-horizontal-left icon'></i>
+                                    <span class="text nav-text">Sections</span>
+                                </a>
+                            </li>
+
+                            <!-----VOTERS------>
+                            <li class="nav-link">
+                                <a href="Voters_Page_Admin.php">
+                                    <i class='bx bxs-group icon'></i>
+                                    <span class="text nav-text">Voters</span>
+                                </a>
+                            </li>
+
+                            <!-----POSITIONS------>
+                            <li class="nav-link">
+                                <a href="Position_Page_Admin.php">
+                                    <i class='bx bxs-objects-horizontal-left icon'></i>
+                                    <span class="text nav-text">Positions</span>
+                                </a>
+                            </li>
+
+                            <!-----CANDIDATES------>
+                            <li class="nav-link">
+                                <a href="Candidates_Page_Admin.php">
+                                    <i class='bx bxs-user-account icon'></i>
+                                    <span class="text nav-text">Candidates</span>
+                                </a>
+                            </li>
+
+                            <!-----BALLOT POSITIONS------>
+                            <li class="nav-link">
+                                <a href="BallotPosition_Page_Admin.php">
+                                    <i class='bx bxs-detail icon'></i>
+                                    <span class="text nav-text">Ballot Position</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+
+                    <!-----BUTTON CONTENT------>
+                    <div class="bottom-content">
+
+                        <!-----LOG OUT------>
+                        <li class="">
+
+                        </li>
+
+                    </div>
+
 
                 </div>
 
 
-                <!-----SIDE BAR------>
-                <nav class="sidebar">
-
-                    <!-----MENU BAR------>
-                    <div class="menu-bar">
-                        <?php
-                        $sql = "SELECT * FROM `setup`";
-                        $result = $conn->query($sql);
-
-                        if (!$result) {
-                            die("Invalid query: " . $conn->error);
-                        } else {
-                            ($row = mysqli_fetch_assoc($result));
-                        }
-                        ?>
-
-                        <!-----SIDEBAR TOP CONTENT-->
-                        <div class="sidebar-content">
-                            <div class="sidebar-top-content">
-
-                                <!------SIKHAY LOGO-->
-                                <div class="sikhay-logo">
-                                    <img src="Organization/<?php echo $row['logo'] ?>" alt="" width="55px">
-                                    <div class="school-name">
-                                        <p style="color: #4A4A4A; font-size: 14px;"><?php echo $row['organization_name']; ?></p>
-                                        <p style="font-weight: lighter; font-size: 13px; color: #9F9898;">Organization</p>
-                                    </div>
-                                </div>
-
-                                <!-----PROFILE ADMIN------>
-                                <header class="sidebar-profile">
-                                    <div class="image-text">
-                                        <?php
-                                        $sql = "SELECT * FROM `admin`";
-                                        $result = $conn->query($sql);
-
-                                        if (!$result) {
-                                            die("Invalid query: " . $conn->error);
-                                        } else {
-                                            ($row = mysqli_fetch_assoc($result))
-
-                                        ?>
-                                            <span class="image">
-                                                <img id="picture-admin" src="Images/<?php echo $row['admin_profile'] ?>" alt="">
-                                            </span>
-                                            <div class="text header-text">
-                                                <p id="name-admin"><?php echo $row['firstname']; ?> <?php echo $row['lastname']; ?></p>
-                                                <span class="name">Online</span>
-                                            </div>
-                                        <?php
-                                        }
-                                        ?>
-                                    </div>
-                                </header>
-
-                            </div>
-                        </div>
-
-
-                        <!-----MENU------>
-                        <div class="menu">
-
-                            <!-----MENU LINKS------>
-                            <ul class="menu-links">
-
-                                <!-----DASHBOARD------>
-                                <li class="nav-link">
-                                    <a href="Dashboard_Page.php">
-                                        <i class='bx bxs-dashboard icon'></i>
-                                        <span class="text nav-text">DashBoard</span>
-                                    </a>
-                                </li>
-
-                                <!-----VOTES------>
-                                <li class="nav-link">
-                                    <a href="Votes_Page_Admin.php">
-                                        <i class='bx bxs-box icon'></i>
-                                        <span class="text nav-text">Votes</span>
-                                    </a>
-                                </li>
-
-                                <!-----Sections------>
-                                <li class="nav-link">
-                                    <a href="Section_Page_Admin.php">
-                                        <i class='bx bxs-objects-horizontal-left icon'></i>
-                                        <span class="text nav-text">Sections</span>
-                                    </a>
-                                </li>
-
-                                <!-----VOTERS------>
-                                <li class="nav-link">
-                                    <a href="Voters_Page_Admin.php">
-                                        <i class='bx bxs-group icon'></i>
-                                        <span class="text nav-text">Voters</span>
-                                    </a>
-                                </li>
-
-                                <!-----POSITIONS------>
-                                <li class="nav-link">
-                                    <a href="Position_Page_Admin.php">
-                                        <i class='bx bxs-objects-horizontal-left icon'></i>
-                                        <span class="text nav-text">Positions</span>
-                                    </a>
-                                </li>
-
-                                <!-----CANDIDATES------>
-                                <li class="nav-link">
-                                    <a href="Candidates_Page_Admin.php">
-                                        <i class='bx bxs-user-account icon'></i>
-                                        <span class="text nav-text">Candidates</span>
-                                    </a>
-                                </li>
-
-                                <!-----BALLOT POSITIONS------>
-                                <li class="nav-link">
-                                    <a href="BallotPosition_Page_Admin.php">
-                                        <i class='bx bxs-detail icon'></i>
-                                        <span class="text nav-text">Ballot Position</span>
-                                    </a>
-                                </li>
-
-                            </ul>
-                        </div>
-
-
-                        <!-----BUTTON CONTENT------>
-                        <div class="bottom-content">
-
-                            <!-----LOG OUT------>
-                            <li class="">
-
-                            </li>
-
-                        </div>
-
-
-                    </div>
-
-
-                </nav>
-            </div>
+            </nav>
+        </div>
         </div>
         <script src="hamburger-navbar.js"></script>
         <script src="Edit_Voters_Profile.js"></script>
